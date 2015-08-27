0e951c57727dd3c66eaa5de9b8c01f6b349e79a6
Matt Fili <matthewjosephfili@gmail.com>
2015-08-27 14:05:42 -0500

    setup basic file structure and loader folder

---
+++ server/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('client'));
+
+app.listen(3000, function () {
+	console.log('Server listening on port 3000');
+});
+
